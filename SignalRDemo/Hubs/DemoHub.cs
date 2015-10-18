0f0fb06b46d37debddef4734ddcf506e6ac11949
Bartosz Czerwonka <bartosz.czerwonka@whallalabs.com>
2015-10-18 10:48:23 +0200

    Init

---
+++ SignalRDemo/Hubs/DemoHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRDemo.Hubs
+{
+    [HubName("DemoHub")]
+    public class DemoHub : Hub
+    {
+        public string Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+            return null;
+        }
+    }
+}
